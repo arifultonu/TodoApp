3202f6fff697333360714b2f72021a983250594d
MD. ARIFUL ISLAM <tonu2050@gmail.com>
2021-12-06 01:45:46 +0600

    Without JWT Changes final

--- src/main/java/com/smefinance/todoapp/common/model/DBData.java
+++ src/main/java/com/smefinance/todoapp/common/model/DBData.java
@@ -4,12 +4,6 @@ import lombok.Data;
 
 @Data
 public class DBData {
-//    public static final String STSWIFT_SCHEMA = "STSWIFT";
-
-    //Local Development Mode Start//
     public static final String CROSS_ORIGIN = "http://localhost:4200";
-
-    //BA Test Production Mode Start//
-//    public static final String CROSS_ORIGIN = "http://192.183.155.121:8083";
 
 }
--- src/main/java/com/smefinance/todoapp/securityconfiguration/jwt/resource/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.smefinance.todoapp.securityconfiguration.jwt.resource;
-
-public class AuthenticationException extends RuntimeException {
-	public AuthenticationException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
