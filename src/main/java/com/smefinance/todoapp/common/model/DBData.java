4967821d6be8ba5b864c97d0a774eb4948e1bc60
MD. ARIFUL ISLAM <tonu2050@gmail.com>
2022-06-12 00:28:43 +0600

    comm

--- src/main/java/com/smefinance/todoapp/common/model/DBData.java
+++ src/main/java/com/smefinance/todoapp/common/model/DBData.java
@@ -6,7 +6,8 @@ import lombok.Data;
 public class DBData {
 //        public static final String CROSS_ORIGIN = "http://localhost";
 //        public static final String CROSS_ORIGIN = "http://localhost:4200";
-    public static final String CROSS_ORIGIN = "http://10.11.201.27:4201";
+//    public static final String CROSS_ORIGIN = "http://10.11.201.27:4201";
+    public static final String CROSS_ORIGIN = "http://localhost:4201";
 
     //AWS EC2 Server
 //    public static final String CROSS_ORIGIN = "http://34.228.16.241";
